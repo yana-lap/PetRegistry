a48711e2f88659def645cd482da3317476c94186
Яна Лапердина <yana.laperdina@gmail.com>
2021-12-09 16:58:47 +0500

    AddingForms2
    Nastya's forms
    
    Co-Authored-By: Anastasia <83288552+puzyreevskaya@users.noreply.github.com>

---
+++ AddPet.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace PetRegistry
+{
+    partial class AddPet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.categoryLabel = new System.Windows.Forms.Label();
+            this.categoryTextBox = new System.Windows.Forms.TextBox();
+            this.genderLabel = new System.Windows.Forms.Label();
+            this.sizeLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.woolLabel = new System.Windows.Forms.Label();
+            this.chippedLabel = new System.Windows.Forms.Label();
+            this.breedTextBox = new System.Windows.Forms.TextBox();
+            this.breedLabel = new System.Windows.Forms.Label();
+            this.NumberTextBox = new System.Windows.Forms.TextBox();
+            this.chippedNameLabel = new System.Windows.Forms.Label();
+            this.typeTextBox = new System.Windows.Forms.TextBox();
+            this.typeLabel = new System.Windows.Forms.Label();
+            this.chippedCheckBox = new System.Windows.Forms.CheckBox();
+            this.ownerTextBox = new System.Windows.Forms.TextBox();
+            this.ownerLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.genderTextBox = new System.Windows.Forms.ComboBox();
+            this.sizeTextBox = new System.Windows.Forms.ComboBox();
+            this.woolTextBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // categoryLabel
+            // 
+            this.categoryLabel.AutoSize = true;
+            this.categoryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.categoryLabel.ForeColor = System.Drawing.Color.CadetBlue;
+            this.categoryLabel.Location = new System.Drawing.Point(40, 69);
+            this.categoryLabel.Name = "categoryLabel";
+            this.categoryLabel.Size = new System.Drawing.Size(107, 20);
+            this.categoryLabel.TabIndex = 4;
+            this.categoryLabel.Text = "Категория";
+            // 
+            // categoryTextBox
+            // 
+            this.categoryTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.categoryTextBox.Location = new System.Drawing.Point(281, 70);
+            this.categoryTextBox.Name = "categoryTextBox";
+            this.categoryTextBox.Size = new System.Drawing.Size(243, 22);
+            this.categoryTextBox.TabIndex = 5;
+            // 
+            // genderLabel
+            // 
+            this.genderLabel.AutoSize = true;
+            this.genderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.genderLabel.ForeColor = System.Drawing.Color.CadetBlue;
+            this.genderLabel.Location = new System.Drawing.Point(40, 153);
+            this.genderLabel.Name = "genderLabel";
+            this.genderLabel.Size = new System.Drawing.Size(45, 20);
+            this.genderLabel.TabIndex = 6;
+            this.genderLabel.Text = "Пол";
+            // 
+            // sizeLabel
+            // 
+            this.sizeLabel.AutoSize = true;
+            this.sizeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sizeLabel.ForeColor = System.Drawing.Color.CadetBlue;
+            this.sizeLabel.Location = new System.Drawing.Point(40, 195);
+            this.sizeLabel.Name = "sizeLabel";
+            this.sizeLabel.Size = new System.Drawing.Size(182, 20);
+            this.sizeLabel.TabIndex = 8;
+            this.sizeLabel.Text = "Размер животного";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nameTextBox.Location = new System.Drawing.Point(281, 28);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(243, 22);
+            this.nameTextBox.TabIndex = 11;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nameLabel.ForeColor = System.Drawing.Color.CadetBlue;
+            this.nameLabel.Location = new System.Drawing.Point(40, 27);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(75, 20);
+            this.nameLabel.TabIndex = 10;
+            this.nameLabel.Text = "Кличка";
+            // 
+            // woolLabel
+            // 
+            this.woolLabel.AutoSize = true;
+            this.woolLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.woolLabel.ForeColor = System.Drawing.Color.CadetBlue;
+            this.woolLabel.Location = new System.Drawing.Point(40, 237);
+            this.woolLabel.Name = "woolLabel";
+            this.woolLabel.Size = new System.Drawing.Size(152, 20);
+            this.woolLabel.TabIndex = 12;
+            this.woolLabel.Text = "Размер шерсти";
+            // 
+            // chippedLabel
+            // 
+            this.chippedLabel.AutoSize = true;
+            this.chippedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chippedLabel.ForeColor = System.Drawing.Color.CadetBlue;
+            this.chippedLabel.Location = new System.Drawing.Point(40, 280);
+            this.chippedLabel.Name = "chippedLabel";
+            this.chippedLabel.Size = new System.Drawing.Size(93, 20);
+            this.chippedLabel.TabIndex = 14;
+            this.chippedLabel.Text = "Есть чип";
+            // 
+            // breedTextBox
+            // 
+            this.breedTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.breedTextBox.Location = new System.Drawing.Point(281, 112);
+            this.breedTextBox.Name = "breedTextBox";
+            this.breedTextBox.Size = new System.Drawing.Size(243, 22);
+            this.breedTextBox.TabIndex = 17;
+            // 
+            // breedLabel
+            // 
+            this.breedLabel.AutoSize = true;
+            this.breedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.breedLabel.ForeColor = System.Drawing.Color.CadetBlue;
+            this.breedLabel.Location = new System.Drawing.Point(40, 111);
+            this.breedLabel.Name = "breedLabel";
+            this.breedLabel.Size = new System.Drawing.Size(80, 20);
+            this.breedLabel.TabIndex = 16;
+            this.breedLabel.Text = "Порода";
+            // 
+            // NumberTextBox
+            // 
+            this.NumberTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.NumberTextBox.Location = new System.Drawing.Point(281, 321);
+            this.NumberTextBox.Name = "NumberTextBox";
+            this.NumberTextBox.Size = new System.Drawing.Size(243, 22);
+            this.NumberTextBox.TabIndex = 19;
+            // 
+            // chippedNameLabel
+            // 
+            this.chippedNameLabel.AutoSize = true;
+            this.chippedNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chippedNameLabel.ForeColor = System.Drawing.Color.CadetBlue;
+            this.chippedNameLabel.Location = new System.Drawing.Point(40, 320);
+            this.chippedNameLabel.Name = "chippedNameLabel";
+            this.chippedNameLabel.Size = new System.Drawing.Size(119, 20);
+            this.chippedNameLabel.TabIndex = 18;
+            this.chippedNameLabel.Text = "Номер чипа";
+            // 
+            // typeTextBox
+            // 
+            this.typeTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.typeTextBox.Location = new System.Drawing.Point(281, 363);
+            this.typeTextBox.Name = "typeTextBox";
+            this.typeTextBox.Size = new System.Drawing.Size(243, 22);
+            this.typeTextBox.TabIndex = 21;
+            // 
+            // typeLabel
+            // 
+            this.typeLabel.AutoSize = true;
+            this.typeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.typeLabel.ForeColor = System.Drawing.Color.CadetBlue;
+            this.typeLabel.Location = new System.Drawing.Point(40, 362);
+            this.typeLabel.Name = "typeLabel";
+            this.typeLabel.Size = new System.Drawing.Size(149, 20);
+            this.typeLabel.TabIndex = 20;
+            this.typeLabel.Text = "Тип владельца";
+            // 
+            // chippedCheckBox
+            // 
+            this.chippedCheckBox.AutoSize = true;
+            this.chippedCheckBox.Location = new System.Drawing.Point(355, 280);
+            this.chippedCheckBox.Name = "chippedCheckBox";
+            this.chippedCheckBox.Size = new System.Drawing.Size(18, 17);
+            this.chippedCheckBox.TabIndex = 22;
+            this.chippedCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ownerTextBox
+            // 
+            this.ownerTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ownerTextBox.Location = new System.Drawing.Point(281, 405);
+            this.ownerTextBox.Name = "ownerTextBox";
+            this.ownerTextBox.Size = new System.Drawing.Size(243, 22);
+            this.ownerTextBox.TabIndex = 24;
+            // 
+            // ownerLabel
+            // 
+            this.ownerLabel.AutoSize = true;
+            this.ownerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ownerLabel.ForeColor = System.Drawing.Color.CadetBlue;
+            this.ownerLabel.Location = new System.Drawing.Point(40, 404);
+            this.ownerLabel.Name = "ownerLabel";
+            this.ownerLabel.Size = new System.Drawing.Size(101, 20);
+            this.ownerLabel.TabIndex = 23;
+            this.ownerLabel.Text = "Владелец";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.CadetBlue;
+            this.label1.Location = new System.Drawing.Point(576, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 20);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Выберите фото";
+            // 
+            // genderTextBox
+            // 
+            this.genderTextBox.FormattingEnabled = true;
+            this.genderTextBox.Location = new System.Drawing.Point(281, 153);
+            this.genderTextBox.Name = "genderTextBox";
+            this.genderTextBox.Size = new System.Drawing.Size(243, 24);
+            this.genderTextBox.TabIndex = 26;
+            // 
+            // sizeTextBox
+            // 
+            this.sizeTextBox.FormattingEnabled = true;
+            this.sizeTextBox.Location = new System.Drawing.Point(281, 195);
+            this.sizeTextBox.Name = "sizeTextBox";
+            this.sizeTextBox.Size = new System.Drawing.Size(243, 24);
+            this.sizeTextBox.TabIndex = 27;
+            // 
+            // woolTextBox
+            // 
+            this.woolTextBox.FormattingEnabled = true;
+            this.woolTextBox.Location = new System.Drawing.Point(281, 237);
+            this.woolTextBox.Name = "woolTextBox";
+            this.woolTextBox.Size = new System.Drawing.Size(243, 24);
+            this.woolTextBox.TabIndex = 28;
+            // 
+            // AddPet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.woolTextBox);
+            this.Controls.Add(this.sizeTextBox);
+            this.Controls.Add(this.genderTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ownerTextBox);
+            this.Controls.Add(this.ownerLabel);
+            this.Controls.Add(this.chippedCheckBox);
+            this.Controls.Add(this.typeTextBox);
+            this.Controls.Add(this.typeLabel);
+            this.Controls.Add(this.NumberTextBox);
+            this.Controls.Add(this.chippedNameLabel);
+            this.Controls.Add(this.breedTextBox);
+            this.Controls.Add(this.breedLabel);
+            this.Controls.Add(this.chippedLabel);
+            this.Controls.Add(this.woolLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.sizeLabel);
+            this.Controls.Add(this.genderLabel);
+            this.Controls.Add(this.categoryTextBox);
+            this.Controls.Add(this.categoryLabel);
+            this.Name = "AddPet";
+            this.Text = "Добавить питомца";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label categoryLabel;
+        private System.Windows.Forms.TextBox categoryTextBox;
+        private System.Windows.Forms.Label genderLabel;
+        private System.Windows.Forms.Label sizeLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label woolLabel;
+        private System.Windows.Forms.Label chippedLabel;
+        private System.Windows.Forms.TextBox breedTextBox;
+        private System.Windows.Forms.Label breedLabel;
+        private System.Windows.Forms.TextBox NumberTextBox;
+        private System.Windows.Forms.Label chippedNameLabel;
+        private System.Windows.Forms.TextBox typeTextBox;
+        private System.Windows.Forms.Label typeLabel;
+        private System.Windows.Forms.CheckBox chippedCheckBox;
+        private System.Windows.Forms.TextBox ownerTextBox;
+        private System.Windows.Forms.Label ownerLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox genderTextBox;
+        private System.Windows.Forms.ComboBox sizeTextBox;
+        private System.Windows.Forms.ComboBox woolTextBox;
+    }
+}
---
+++ LoginForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace PetRegistry
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.logInButton = new System.Windows.Forms.Button();
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Location = new System.Drawing.Point(220, 125);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(205, 22);
+            this.loginTextBox.TabIndex = 0;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(220, 196);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(205, 22);
+            this.passwordTextBox.TabIndex = 1;
+            // 
+            // logInButton
+            // 
+            this.logInButton.BackColor = System.Drawing.SystemColors.Window;
+            this.logInButton.Location = new System.Drawing.Point(259, 261);
+            this.logInButton.Name = "logInButton";
+            this.logInButton.Size = new System.Drawing.Size(122, 41);
+            this.logInButton.TabIndex = 2;
+            this.logInButton.Text = "Войти";
+            this.logInButton.UseVisualStyleBackColor = false;
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.loginLabel.Location = new System.Drawing.Point(216, 89);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(70, 20);
+            this.loginLabel.TabIndex = 3;
+            this.loginLabel.Text = "Логин:";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.passwordLabel.Location = new System.Drawing.Point(217, 167);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(84, 20);
+            this.passwordLabel.TabIndex = 4;
+            this.passwordLabel.Text = "Пароль:";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(634, 383);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.loginLabel);
+            this.Controls.Add(this.logInButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.loginTextBox);
+            this.Name = "LoginForm";
+            this.Text = "Авторизация";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button logInButton;
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.Label passwordLabel;
+    }
+}
+
